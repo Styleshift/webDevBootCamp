db452cf955fed6ea721be0cc8076a9949db243c2
Keenan <42125735+Styleshift@users.noreply.github.com>
2023-02-22 21:45:43 -0500

    added musuem of candy project

---
+++ bootstrap4_Code/13_Museum_Of_Candy/Starter/js/javascript.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(document).scroll(function() {
+        const $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
